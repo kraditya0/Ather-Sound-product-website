42d257f12f873c50bb13120529b89c5465f0f282
adityakro <Adityake2217@gmail.com>
2026-06-04 22:57:58 +0530

    Fallback to SQLite in production if DATABASE_URL is missing

--- app/config.py
+++ app/config.py
@@ -44,7 +44,7 @@ class TestingConfig(Config):
 
 class ProductionConfig(Config):
     DEBUG = False
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', f"sqlite:///{os.path.join(basedir, '..', 'instance', 'app.db')}")
 
 config_by_name = {
     'development': DevelopmentConfig,
